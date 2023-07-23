37a81328650206d85af94a432d2093429b42148b
Kusumsiri <kusumsiri@users.noreply.github.com>
2023-07-23 18:04:55 +0530

    Update 03-variables.tf

--- 03-variables.tf
+++ 03-variables.tf
@@ -13,11 +13,6 @@ variable "hello_app_name" {
   default = "http-svc"
 }
 
-variable "host_name" {
-  description = "The host name"
-  default = "vpn3.eurolockers.com"
-}
-
 variable "kube-version" {
   description = "The kube-version"
   default = "36.2.0"
