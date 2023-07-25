63e22c68ae44b0934a6530b64cd79cfbaf07c0ee
Kusumsiri <kusumsiri@users.noreply.github.com>
2023-07-26 05:22:51 +0530

    edited

--- 01-providers.tf
+++ 01-providers.tf
@@ -7,3 +7,14 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  # config_context = "minikube"
+  config_context = "microk8s"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
