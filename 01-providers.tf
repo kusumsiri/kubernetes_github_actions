55f7e9b617e041848805e4401e95b8fb103ffae2
kusumsiri <kusumsiri@gmail.com>
2023-07-27 16:57:55 +0530

    Edited

--- 01-providers.tf
+++ 01-providers.tf
@@ -15,16 +15,6 @@ terraform {
   }
 }
 
-resource "aws_dynamodb_table" "state_locking" {
-  hash_key = "LockID"
-  name     = "terraform-state-locking"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  billing_mode = "PAY_PER_REQUEST"
-}
-
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   # config_context = "minikube"
--- 02-main.tf
+++ 02-main.tf
@@ -3,3 +3,13 @@ resource "kubernetes_namespace" "namespace" {
     name = var.k8_namespace
   }
 }
+
+resource "aws_dynamodb_table" "state_locking" {
+  hash_key = "LockID"
+  name     = "terraform-state-locking"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
