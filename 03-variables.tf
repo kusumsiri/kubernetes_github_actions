cbec7d1b684106957e34e5d4b141ff0bcbac0dc8
kusumsiri <kusumsiri@gmail.com>
2023-07-28 05:19:31 +0530

    App name changed

--- 03-variables.tf
+++ 03-variables.tf
@@ -5,10 +5,10 @@ variable "k8_namespace" {
 
 variable "hello_app_name" {
   description = "The hello-app name"
-  default = "my-hello-all"
+  default = "my-hello-app"
 }
 
 variable "external_ips"{
   description = "External IPs to set on LoadBalancer" 
-  default = ["195.201.150.42"]
+  default = ["1.2.3.4"]
 }
