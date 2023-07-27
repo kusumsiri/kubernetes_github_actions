d8d61f7472fa4c8d2ff57e2d671c4942f6e0ab56
kusumsiri <kusumsiri@gmail.com>
2023-07-28 05:23:02 +0530

    removed helm provider

--- 01-providers.tf
+++ 01-providers.tf
@@ -26,12 +26,6 @@ provider "kubernetes" {
   config_context = "microk8s"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
