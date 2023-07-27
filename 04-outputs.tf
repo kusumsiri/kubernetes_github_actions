ef02c134a32fd39716dea505d347af0b73a10876
kusumsiri <kusumsiri@gmail.com>
2023-07-27 17:39:10 +0530

    Edited

--- 04-outputs.tf
+++ 04-outputs.tf
@@ -1,9 +1,9 @@
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.state_locking.name
-  description = "The ARN of the DynamoDB table"
-}
+# output "dynamodb_table_name" {
+#   value       = aws_dynamodb_table.state_locking.name
+#   description = "The ARN of the DynamoDB table"
+# }
 
-output "dynamodb_table_arn" {
-  value       = aws_dynamodb_table.state_locking.arn
-  description = "The ARN of the DynamoDB table"
-}
+# output "dynamodb_table_arn" {
+#   value       = aws_dynamodb_table.state_locking.arn
+#   description = "The ARN of the DynamoDB table"
+# }
