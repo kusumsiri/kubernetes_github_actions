fc0e01584d2ad04319a6aea8aa8e698f30671a06
kusumsiri <kusumsiri@gmail.com>
2023-07-27 12:46:51 +0530

    Adding

--- 01-providers.tf
+++ 01-providers.tf
@@ -5,6 +5,24 @@ terraform {
       version = "2.11.0"
     }
   }
+
+  backend "s3" {
+    bucket         	   = "terraform-state-kusumsiri" 
+    key              	 = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+    dynamodb_table     = "terraform-state-locking"
+  }
+}
+
+resource "aws_dynamodb_table" "state_locking" {
+  hash_key = "LockID"
+  name     = "terraform-state-locking"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
 }
 
 provider "kubernetes" {
